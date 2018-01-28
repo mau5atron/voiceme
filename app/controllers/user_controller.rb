0f47eb2e66b5797e299e20504db3b44c87221f83
mau5atron <gabrielbetancourt96@outlook.com>
2018-01-28 15:01:13 -0800

    updated user controller to have crud actions

--- app/controllers/user_controller.rb
+++ app/controllers/user_controller.rb
@@ -4,4 +4,43 @@ class UserController < ApplicationController
 		render json: @users
 	end
 	
+	def show
+		render json: @user
+	end
+
+	def create
+		@user = User.new(user_params)
+
+		if @user.save
+			render json: @user, status: :created, location: @user
+		else
+			render  json: @user.errors, status: :unprocessable_entity
+		end
+	end
+
+	def update
+		if @user.update(user_params)
+			render json: @user
+		else
+			render json: @user.errors, status: :unprocessable_entity
+		end
+	end
+
+	def destroy
+		@user.destroy
+	end
+
+	private 
+	# strong params area for user_params
+
+	def set_user
+		@user = User.find(params[:id])
+	end
+
+	def user_params
+		params.permit(:name, :email)
+	end
+
+
+
 end
