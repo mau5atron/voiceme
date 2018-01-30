539587e59ef77bb03f76a221bfdd33a56db306a3
mau5atron <gabrielbetancourt96@outlook.com>
2018-01-29 17:41:12 -0800

    added validations to memo model, checking for presence of title, date, text_body, and user_id

--- app/models/memo.rb
+++ app/models/memo.rb
@@ -1,3 +1,7 @@
 class Memo < ApplicationRecord
 	belongs_to :user
+	validates :title, presence: true
+	validates :text_body, presence: true
+	validates :date, presence: true
+	validates :user_id, presence: true
 end
