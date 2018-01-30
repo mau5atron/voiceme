e04267b903e04119c2ed174a75f6c2dc708654a3
mau5atron <gabrielbetancourt96@outlook.com>
2018-01-29 17:28:04 -0800

    added test validations for name:, email: // name:, email: cannot be nil

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -1,5 +1,22 @@
 require 'rails_helper'
 
 RSpec.describe User, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  # pending "add some examples to (or delete) #{__FILE__}"
+
+  describe "Validation" do
+  	it "is valid with valid attributes" do
+  	user = User.new(name: "Johnny boy", email: "johnnyboy@gmail.com")
+  	end
+
+  	it "is invalid without a name" do
+  		bad_user = User.new(name: nil, email: "test@gmail.com")
+  		expect(bad_user).to_not be_valid
+  	end
+
+  	it "is invalid without an email" do
+  		bad_user = User.new(name: "Johnny", email: nil)
+  		expect(bad_user).to_not be_valid
+  	end
+  end
+  
 end
