f9ca9c66f45eff0e661066c376bf7785247edecb
mau5atron <gabrielbetancourt96@outlook.com>
2018-01-31 13:54:07 -0800

    updated user spec test to include required password not be nil

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -5,18 +5,23 @@ RSpec.describe User, type: :model do
 
   describe "Validation" do
   	it "is valid with valid attributes" do
-  	user = User.new(name: "Johnny boy", email: "johnnyboy@gmail.com")
+  	user = User.new(name: "Johnny boy", email: "johnnyboy@gmail.com", password: "test")
   	end
 
   	it "is invalid without a name" do
-  		bad_user = User.new(name: nil, email: "test@gmail.com")
+  		bad_user = User.new(name: nil, email: "test@gmail.com", password: "test")
   		expect(bad_user).to_not be_valid
   	end
 
   	it "is invalid without an email" do
-  		bad_user = User.new(name: "Johnny", email: nil)
+  		bad_user = User.new(name: "Johnny", email: nil, password: "test")
   		expect(bad_user).to_not be_valid
   	end
+
+  	it "is invalid without a password" do
+  		bad_user = User.new(name: "Johnny boy", email: "johnnyboy@gmail.com", password: nil)
+  		expect(bad_user).to_not be_valid
+   	end
   end
 
   describe "Associations" do 
