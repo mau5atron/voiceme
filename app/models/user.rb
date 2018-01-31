5509da86eb72cce57d367a4b12ef5af847862271
mau5atron <gabrielbetancourt96@outlook.com>
2018-01-31 00:48:41 -0800

    updated user model file with actions required for running new methods relating to password and user authentication // before a user is saved to the db, their password is encrypted // before creating a new user a unque token is generated

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,6 +2,15 @@ class User < ApplicationRecord
 	has_many :memos
 	validates :name, presence: true
 	validates :email, presence: true, uniqueness: true
+	attribute :password, :string	
+
+	# before a user is saved to db their password is encrypted 
+	before_save :encrypt_password
+	
+	# before creating a user a unique token is created that is tied to their User
+	before_create :generate_token
+	
+	
 
 	# used for login
 	# method searches for user with specific email in database and checks to make
@@ -12,7 +21,7 @@ class User < ApplicationRecord
 		if user && user.password_hash == BCrypt::Engine.hash_secret(password, user.password_salt)		
 			user
 		else
-			# else returned nil
+			# else returns nil
 			nil
 		end
 	end
