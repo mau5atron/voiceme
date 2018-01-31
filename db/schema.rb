4609c0cb65d7d6810f542d20cf2115386a3fc3e2
mau5atron <gabrielbetancourt96@outlook.com>
2018-01-30 23:59:48 -0800

    updated schema - ran rake db:migrate

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20180128222443) do
+ActiveRecord::Schema.define(version: 20180131075231) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -30,6 +30,10 @@ ActiveRecord::Schema.define(version: 20180128222443) do
     t.string "email"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.string "token"
+    t.string "password_hash"
+    t.string "password_salt"
+    t.index ["token"], name: "index_users_on_token"
   end
 
   add_foreign_key "memos", "users"
